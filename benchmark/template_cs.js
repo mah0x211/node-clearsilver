67666bceea381f96d4fab35c9d8ee41d89d64282
Masatoshi Teruya <mah0x211@gmail.com>
2011-10-31 13:29:55 +0900

    add: file cache, RenderFile method

--- benchmark/template_cs.js
+++ benchmark/template_cs.js
@@ -1,25 +1,61 @@
 var util = require('util'),
-	fs = require('fs'),
-	ClearSilver = require('ClearSilver');
+fs = require('fs'),
+ClearSilver = require('ClearSilver'),
+cs = new ClearSilver(),
+filepath = __dirname + '/hello_cs.txt',
+RENDER_TYPE = process.argv[2],
+RENDER_COUNT = ( Number( process.argv[3] ) ) ? +process.argv[3] : 0;
 
-var cs = new ClearSilver;
-
-function Render( tmpl )
+function RenderString( tmpl )
 {
-	var n = ( Number( process.argv[2] ) ) ? +process.argv[2] : 0;
-	var ms = +new Date();
-	
-	for( var i = 0; i < n; i++ ){
-		cs.renderString( tmpl, { items:['foo', 'bar', 'baz'] } );
-	}
-	console.log( +new Date() - ms + 'ms' + "\n" + util.inspect( process.memoryUsage() ) );	
+    fs.readFile( tmpl, function( err, data )
+		{
+		if( err ){
+		throw err;
+		}
+		else
+		{
+		var ms = +new Date();
+		
+		for( var i = 0; i < RENDER_COUNT; i++ ){
+		cs.RenderString( data, { items:['foo', 'bar', 'baz'] } );
+		}
+		console.log( 'RenderString[' + i + ']: ' + ( +new Date() - ms ) + 'ms' + "\n" + util.inspect( process.memoryUsage() ) );
+		// console.log( cs.CACHE );
+		}
+		});
 }
 
-fs.readFile( __dirname + '/hello_cs.txt', function( err, data ){
-	if( err ){
-		throw err;
-	}
-	else {
-		Render( data.toString() );
-	}
-} );
+function RenderFile( tmpl )
+{
+    var ms = +new Date(),
+    cnt = 0,
+    render = function()
+    {
+	cs.RenderFile( tmpl, { items:['foo', 'bar', 'baz'] }, function( err, str )
+		      {
+		      if( err ){
+		      throw err;
+		      }
+		      if( ++cnt === RENDER_COUNT ){
+		      console.log( 'RenderFile[' + cnt + ']: ' + ( +new Date() - ms ) + 'ms' + "\n" + util.inspect( process.memoryUsage() ) );
+		      // console.log( cs.CACHE );
+		      }
+		      else {
+		      render();
+		      }
+		      });
+    };
+    
+    render();
+}
+
+if( RENDER_TYPE === 'string' ){
+    RenderString( filepath );
+}
+else if( RENDER_TYPE === 'file' ){
+    RenderFile( filepath );
+}
+else {
+    console.log( 'unknown render type: ' + RENDER_TYPE );
+}
--- install.sh
+++ install.sh
@@ -6,19 +6,34 @@ SHPLIB=streamhtmlparser-0.1.tar.gz
 if [ -d $CSLIBS ]; then 
     rm -Rf $CSLIBS
 fi
+echo "svn checkout http://clearsilver.googlecode.com/svn/trunk/ $CSLIBS"
 svn checkout http://clearsilver.googlecode.com/svn/trunk/ $CSLIBS
 
 echo "cd $CSLIBS"
 cd $CSLIBS
+
 CSLIBS=`pwd`
+echo 'mkdir ./libs'
 mkdir ./libs
+
+echo 'wget http://streamhtmlparser.googlecode.com/files/streamhtmlparser-0.1.tar.gz'
 wget http://streamhtmlparser.googlecode.com/files/streamhtmlparser-0.1.tar.gz
+
+echo 'tar xvzf streamhtmlparser-0.1.tar.gz'
 tar xvzf streamhtmlparser-0.1.tar.gz
+
+echo 'mv ./streamhtmlparser-0.1 ./streamhtmlparser'
 mv ./streamhtmlparser-0.1 ./streamhtmlparser
+
+echo './autogen.sh'
 ./autogen.sh
+
+echo "./configure --prefix=$CSLIBS --disable-apache --disable-python --disable-perl --disable-ruby --disable-java --disable-csharp"
 ./configure --prefix=$CSLIBS --disable-apache --disable-python --disable-perl --disable-ruby --disable-java --disable-csharp
-make depend
-make && make man && make install
+make depend && make && make man && make install
+echo 'cd ../'
 cd ../
-node-waf configure --clearsilver=$CSLIBS/lib --clearsilver-includes=$CSLIBS/includes
+
+CSLIBS=`pwd`
+node-waf configure --clearsilver=$CSLIBS/cslibs/lib --clearsilver-includes=$CSLIBS/cslibs/include
 node-waf build
