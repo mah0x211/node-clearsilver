a1d50144399d92375c69a09f4d72782fb6a00381
Masatoshi Teruya <mah0x211@gmail.com>
2012-02-02 21:30:51 +0900

    add: index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ binding to ClearSilver
+ author: masatoshi teruya
+ email: mah0x211@gmail.com
+ copyright (C) 2012, masatoshi teruya. all rights reserved.
+*/
+
+module.exports = require( __dirname + '/build/default/clearsilver').ClearSilver;
